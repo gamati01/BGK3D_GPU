495dc144e9482570c1e2975e4dd07bd5e637a069
GIORGIO AMATI <gamati01@dgxslurm.m100.cineca.it>
2024-01-17 21:43:46 +0100

    added TKE

--- SRC/diagno.F90
+++ SRC/diagno.F90
@@ -34,6 +34,7 @@
        integer             :: i,j,k
 !
        real(mykind):: rtot,xtot,ytot,ztot,stot
+       real(mykind):: tke
        real(mykind):: rho,rhoinv
        real(mykind):: x01,x02,x03,x04,x05,x06,x07
        real(mykind):: x08,x09,x10,x11,x12,x13
@@ -47,6 +48,7 @@
        cte1 = uno
 #endif
 !
+       tke  = zero
        rtot = zero
        xtot = zero
        ytot = zero
@@ -119,6 +121,52 @@
        write(63,1004) itime, xtot, ytot, ztot, rtot, stot
        flush(63)
 !
+! conmpute turbulent kinetic energy
+       do k=1,n
+         do j=1,m
+            do i=1,l
+               x01 = a01(i,j,k)
+               x02 = a02(i,j,k)
+               x03 = a03(i,j,k)
+               x04 = a04(i,j,k)
+               x05 = a05(i,j,k)
+               x06 = a06(i,j,k)
+               x07 = a07(i,j,k)
+               x08 = a08(i,j,k)
+               x09 = a09(i,j,k)
+               x10 = a10(i,j,k)
+               x11 = a11(i,j,k)
+               x12 = a12(i,j,k)
+               x13 = a13(i,j,k)
+               x14 = a14(i,j,k)
+               x15 = a15(i,j,k)
+               x16 = a16(i,j,k)
+               x17 = a17(i,j,k)
+               x18 = a18(i,j,k)
+               x19 = a19(i,j,k)
+
+               rho = x01 + x02 + x03 + x04 + x05 + x06 + x07 + x08 &
+                    +x09 + x10 + x11 + x12 + x13 + x14 + x15 + x16 &
+                    +x17 + x18 + x19 + cte1
+!
+               rhoinv = uno/rho
+!
+               xj = (x01+x02+x03+x04+x05-x10-x11-x12-x13-x14)*rhoinv
+               yj = (x03+x07+x08+x09+x12-x01-x10-x16-x17-x18)*rhoinv
+               zj = (x04+x06+x07+x13+x18-x02-x09-x11-x15-x16)*rhoinv
+
+               tke = tke + 0.5*((xtot - xj)*(xtot - xj) + & 
+                                (ytot - yj)*(ytot - yj) + & 
+                                (ztot - zj)*(ztot - zj))  
+
+
+           enddo
+         enddo
+       enddo
+
+       write(666,*) itime, tke/(float(l)*float(m)*float(n))
+       flush(666)
+
 #ifdef DEBUG_1
        if (myrank == 0) then
           write(6,*) "DEBUG1: Exiting from sub. diagno"
